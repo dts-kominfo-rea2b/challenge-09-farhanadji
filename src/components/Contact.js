d488ce157ac4bd8557539da63d7ba80c03999f8f
Farhan Adji <farhanadji@live.com>
2022-06-26 21:57:59 +0700

    Answer

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,10 +2,20 @@
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
 
-const Contact = () => {
+const Contact = ({data}) => {
+    const { name, phone, email, photo } = data;
     return (
-        <></>
-    )
+        <div className="contact-card">
+            <div className="contact-image">
+                <img src={ photo } alt={ "contact-" + name } />
+            </div>
+            <div className="contact-detail">
+                <p className="contact-name">{ name }</p>
+                <p className="contact-phone">{ phone }</p>
+                <p className="contact-email">{ email }</p>
+            </div>
+        </div>
+    );
 }
 
 export default Contact;
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,10 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <></>
+        <div className="header">
+            <h1 className="header-title">Call a Friend</h1>
+            <h3 className="header-subtitle">your friendly contact app</h3>
+        </div>
     )
 }
 
